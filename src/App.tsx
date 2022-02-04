168c216cd68a7e7a999b99b041ecd2ac26e94b5d
MAC <bashorunbethel99@gmail.com>
2022-02-04 00:41:10 -0800

    submit commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {useDispatch, useSelector} from "react-redux"
 import { RootStore } from './store';
 import { GetBlogPost } from './actions/blogPostAction';
+import { Row, Col, Card} from 'react-bootstrap';
+
 
 
 function App() {
 const dispatch = useDispatch()
-  const [blogPostName, setBlogPostName] = useState("")
+  const [blogPostName, setBlogPostName] = useState(" ")
+  const [search, setSearch]: [string, (search: string) => void] = React.useState("");
+
   const blogPostState = useSelector((state: RootStore) => state.blogPost);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setBlogPostName(event.target.value);
   const handleSubmit = () => dispatch(GetBlogPost(blogPostName))
@@ -21,29 +26,53 @@ const dispatch = useDispatch()
       <div className="header">
        <p className="blog_app">Blog app Test</p>
       </div>
-      <input className="input" type="text" onChange={handleChange} />
-      <button className="button"onClick={handleSubmit}>Search</button>
+      {/*<UserFind/>*/}
+      <div className="body-wrap">
+        {/*<div className="input_type">
+          <input className="input" type="text" onChange={handleChange} placeholder="Filter" />
+          <button className="button" onClick={handleSubmit}>Search</button>
+        </div>*/}
+
+     
+
+     
       {/*{blogPostState === undefined ? <h1>Loading...</h1> :
 
         console.log('data: ', blogPostState)
 }*/}
      
       {blogPostState.blogPost !== undefined && (<div>
-        {blogPostState.blogPost.map((post,index) => {
-          return (
-            <div key={index}>
-              <p>{post.title}</p>
-              <p>{post.body}</p>
-
-              {/*<p> {post.id}</p>*/}  
-              {/*<p>{post.userId}</p>*/}
+          {blogPostState.blogPost.map((post, index) => {
+            //if (search === "" || post.body.toLowerCase().includes(search.toLowerCase())) {
+              return (
+                <div className="body_item" key={index}>
+           
+                  <Row xs={1} md={4} className="g-10">
+                    {Array.from({ length: 4 }).map((_, idx) => (
+                      <Col>
+                        <Card className="card">
+                          <Card.Img variant="top" src="https://www.istockphoto.com/photo/aerial-view-of-residential-distratic-at-major-mackenzie-dr-and-islinton-ave-detached-gm1320991884-407359315" />
+                          <Card.Body>
+                            <Card.Title className="">Title: {post.title}</Card.Title>
+                            <Card.Text>
+                              Body:  {post.body}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    ))}
+                  </Row>
+                  {/*<p>{post.userId}</p>*/}
+                  {/*<p> {post.id}</p>*/}
              
-             
-            </div>
+                </div>
 
-          )  
-        })}
-     </div>)}
+              )
+            //} 
+        })
+        }
+        </div>)}
+        </div>
       
     </div>
   );
--- src/components/userFind.tsx
+++ src/components/userFind.tsx
@@ -4,7 +4,11 @@ import React from "react"
 
 const UserFind:React.FC = () => {
     return (
-        <div></div>
+        <div>
+            <div className="input_wrapper"></div>
+            <input type="text" placeholder="filter" />
+            <button>Search</button>
+        </div>
     )
 }
 
